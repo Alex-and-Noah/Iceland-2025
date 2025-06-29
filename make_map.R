66fae4c9fbeafa186113697f35509b9823799ed4
Alex McSween <alexmcsw@gmail.com>
2025-06-29 17:34:27 -0400

    grab primary roads and geojson of westfjords

--- make_map.R
+++ make_map.R
@@ -4,56 +4,47 @@ library("osmdata")
 library("sf")
 
 # the bounding box, limiting what we fetch
-location <- getbb("reykjavik iceland", featuretype = "city")
+location <- getbb("iceland")
 
 # the big streets
-dus_streets <- location %>% 
+streets <- location %>% 
   opq() %>% 
   add_osm_feature(key = "highway", 
                   value = c("motorway", "trunk",  "primary", 
                             "secondary", "tertiary")) %>% 
   osmdata_sf()
 
-small_streets <- location %>% 
-  opq() %>% 
-  add_osm_feature(
-    key = "highway", 
-    value = c("residential", "living", "unclassified", 
-              "service", "footway")) %>% 
-  osmdata_sf()
-
-boundaries <- location %>%
-  opq() %>%
-  add_osm_feature(key = "admin_level", value = "6") %>%
-  osmdata_sf()
-
-background <- boundaries$osm_multipolygons %>%
-  filter(name != "Reykjavíkurborg")
-
-xlimit <- c(-21.98, -21.5)
-ylimit <- c(64, 64.5)
+xlimit <- c(-25, -21)
+ylimit <- c(65, 67)
 xmid <- xlimit[1] + diff(xlimit) / 2 
 ratio <- diff(xlimit) / diff(ylimit)
 
 
+#geojsson
+
+# Geospatial data available at the geojson format
+tmp_geojson <- tempfile(fileext = ".geojson")
+
+download.file(
+"https://raw.githubusercontent.com/baldurh/iceland-geodata/master/country/100/iceland.geojson",  tmp_geojson
+)
+
+
+my_sf <- read_sf(tmp_geojson)
+my_sf <- st_transform(my_sf, crs = st_crs(streets$osm_lines))
+
+
+
+
 
 map <- ggplot() +
-  # small streets
-  geom_sf(data = small_streets$osm_lines, alpha = .6,
-          linewidth = .2, colour = "grey30") +
+  geom_sf(data = my_sf, fill = "#e9dfbe", color = "transparent") +
   # large steets, making them stand out a bit with colour
-  geom_sf(data = dus_streets$osm_lines, alpha = .8,
-          linewidth = .4, colour = "goldenrod4") +
-  # adding the background
-  # need to increase size here, otherwise there will be a few semi-cutoff
-  # streets directly at the city border
-  geom_sf(data = background, colour = "grey10",
-    linewidth = 1.5, fill = "grey10") +
+  geom_sf(data = streets$osm_lines, alpha = .8,
+          linewidth = .6, colour = "#8b1414") +
   # setting limits
   coord_sf(ylim = ylimit, xlim = xlimit, expand = FALSE) +
   # finishing touches
-  theme_void() +
-  theme(panel.background = element_rect(fill = "grey10"),
-        plot.background = element_rect(fill = "grey10"))
+  theme_void()
 
 map
